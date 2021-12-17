b32ab7bc8dec52455e0e98e3765858a1e48bd7ab
SurajMali <76955549+SurajMali002@users.noreply.github.com>
2021-12-17 18:57:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDxsSeYUvhL3n9WBVzzPe7rZ8ME96y0D8I",
+  authDomain: "spectogram-9479e.firebaseapp.com",
+  projectId: "spectogram-9479e",
+  storageBucket: "spectogram-9479e.appspot.com",
+  messagingSenderId: "745128676780",
+  appId: "1:745128676780:web:d7a0713e250e48dda1be09"
+};
